ab261040fd9093de91af4d8e121fad6cda92ea4c
levi_wasa <leviwasa@gmail.com>
2026-01-13 21:23:01 +0200

    added card setting

--- students-admin/src/components/Card.jsx
+++ students-admin/src/components/Card.jsx
@@ -4,9 +4,18 @@ import {useState} from 'react'
 // Components section
 export default function Card({id,name,age,major,email,onDelete}){
 
+    /*
+        continue on the setting edit/delete section
+    */
     return(
         <div id={id} className="card shadow-sm student-card">
-            <span className="card-del" onClick={()=>onDelete(id)}>✖️</span>
+            <div className="settings">
+                <i className="uil uil-ellipsis-h">➕</i>
+                <ul className="student-edit-manu">
+                    <li><span className="card-edit">edit</span></li>
+                    <li><span className="card-del" onClick={()=>onDelete(id)}>delete</span></li>
+                </ul>
+            </div>
             <div className="card-body">
                 <h5 className="card-title text-center mb-3">Student Card</h5>
                 <p className="card-text"><strong>Name:</strong> {name}</p>
